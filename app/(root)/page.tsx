fa4bbcd685bae524a48aa6066233922de5298b2a
Kehinde Orilogbon <archkenshin@gmail.com>
2023-12-18 01:14:11 -0500

    hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,35 @@
+import {Button} from "@/components/ui/button"
 import Image from 'next/image'
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello Next 14</h1>
-    </main>
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">
+              Host, Connect, Celebrate: Your Events, Our Platform!
+            </h1>
+            <p className="p-regular-20 md:p-regular-24">
+              Book and learn about the best venues and vendors for your events. Your events will have a lasting impact!
+            </p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">Explore now</Link>
+            </Button>
+          </div>
+          <Image src="/assets/images/hero.png" width={1000} height={1000} alt="hero" className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]" />
+        </div>
+      </section>
+      <div id="events" className="wrapper my-a fle flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">
+          Trusted by<br />tThousands of Events
+        </h2>
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          Search
+          CategoryFilter
+        </div>
+      </div>
+    </>
   )
 }
